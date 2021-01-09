6af53ebe93d5a1399bbd73818ebc86b9e1e029dc
Aaron Pop <FezFamiliar@yahoo.com>
2021-01-09 13:32:51 -0800

    Add files via upload

---
+++ w-appointments/index.php
@@ -0,0 +1,51 @@
+<?php
+
+$start = 8;
+$end = 19;
+
+
+if(isset($_POST['date']) && isset($_POST['time']))
+{
+	// check if the interval isn't taken --> if not book him else dont
+
+}
+else if(isset($_POST['date']) && !isset($_POST['time']))
+{
+	// you need to fill in time
+}
+else if(!isset($_POST['date']) && isset($_POST['time']))
+{
+	// you need to fill in date
+}
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>W-appointment</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<main>
+		<form method="POST" disabled>
+			
+			Date: <input type="date" name="date">
+			<br><br>
+			Time:
+			<select name="time">
+				<?php for($i = $start; $i <= $end; ++$i): ?>
+
+					<option value=<?php echo $i; ?>><?php echo $i . ':00 - ' . $i + 1 . ':00' ?></option>
+
+				<?php endfor; ?>
+			</select>
+			<br><br>
+			<input type="submit" value="book">	
+		</form>
+	</main>
+</body>
+</html>
